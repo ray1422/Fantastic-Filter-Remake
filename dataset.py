dd14ac244b12f4fd864de330c3c150d76883e09a
Ray1422 <ray20020809@gmail.com>
2020-04-12 18:48:19 +0800

    update dataset cache mode

--- dataset.py
+++ dataset.py
@@ -3,6 +3,7 @@ import os
 
 import numpy
 import tensorflow as tf
+from tensorflow_core.python.data.ops.dataset_ops import AUTOTUNE
 
 
 def get_dataset(dir_path, batch_size=32):
@@ -16,14 +17,19 @@ def get_dataset(dir_path, batch_size=32):
         image -= 1  # normalize to [-1, 1] range
         return image
 
+    def preprocess(x):
+        x = tf.map_fn(open_image, x)
+        return x
+
     filenames = [os.path.basename(f) for f in glob.glob(f"{dir_path}/x/*g")]  # jpg, jpeg or png LOL
     x_dir = f"{dir_path}/x"
     y_dir = f"{dir_path}/y"
     x_files = [f'{x_dir}/{i}' for i in filenames]
     y_files = [f'{y_dir}/{i}' for i in filenames]
-    x_dataset = tf.data.Dataset.from_tensor_slices(x_files).map(open_image)
-    y_dataset = tf.data.Dataset.from_tensor_slices(y_files).map(open_image)
+    x_dataset = tf.data.Dataset.from_tensor_slices(x_files)
+    y_dataset = tf.data.Dataset.from_tensor_slices(y_files)
     dataset = tf.data.Dataset.zip((x_dataset, y_dataset))
-    dataset = dataset.cache().shuffle(buffer_size=len(filenames)).repeat().batch(batch_size=batch_size)
+    dataset = dataset.cache().shuffle(buffer_size=len(filenames)).repeat().batch(batch_size=batch_size) \
+        .map(preprocess).prefetch(AUTOTUNE)
 
     return dataset, len(filenames)
--- train.py
+++ train.py
@@ -4,10 +4,10 @@ from models import get_generator, get_discriminator, GANModel
 if __name__ == '__main__':
     gan = GANModel(generator=get_generator(), discriminator=get_discriminator())
     gan.compile()
-    train_dataset, train_steps = get_dataset("dataset/train", batch_size=8)
-    valid_dataset, valid_steps = get_dataset("dataset/valid", batch_size=8)
+    train_dataset, train_steps = get_dataset("dataset/train", batch_size=16)
+    valid_dataset, valid_steps = get_dataset("dataset/valid", batch_size=16)
     gan.fit(train_dataset,
             steps_pre_epoch=train_steps,
-            epochs=5,
+            epochs=1,
             valid_dataset=valid_dataset,
             valid_steps=valid_steps)
